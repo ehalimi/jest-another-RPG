c56654c2396d63758a2a61b11e5dd7aa7587fb8f
Ema Halimi <ehalimi@na.edu>
2022-07-29 18:36:26 -0500

    created a player object

---
+++ __tests__/player.test.js
@@ -0,0 +1,18 @@
+const Player = require('../lib/player');
+const Potion = require('../lib/potion');
+
+jest.mock('../lib/potion');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
+
+console.log(new Potion());
